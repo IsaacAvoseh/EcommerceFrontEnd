18c7e8514e1ce455610cc1f78295a8ca12090fbe
IsaacAvoseh <avosehisaac0@gmail.com>
2022-02-18 09:20:58 +0100

    fixed some bugs

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -77,7 +77,7 @@ useEffect(() => {
                 if (data.status === "error" || data.errors) {
                     swal('Error', data.message, 'error')
                     return;
-                } else {
+                } else if( !data.data === null || !data.data === undefined){
                  toast.success('Login Successful')
                     localStorage.setItem("token", JSON.stringify(data.token));
                     localStorage.setItem("user-info", JSON.stringify(data.data));
@@ -93,6 +93,7 @@ useEffect(() => {
             })
             .catch((err) => {
                 console.log(err);
+                return;
             });
     };
 
