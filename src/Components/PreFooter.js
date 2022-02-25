abf0781113e27ecf4a0a40e6dbbaae46ddcac476
IsaacAvoseh <avosehisaac0@gmail.com>
2022-02-25 15:33:19 +0100

    added readme

--- src/Components/PreFooter.js
+++ src/Components/PreFooter.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PreFooter() {
     return (
         <div className="supporter">
-        <img src="/images/supporters.jpg" alt="" />
+            <img src="https://github.com/IsaacAvoseh/EcommerceFrontEnd/blob/d45b301edcd27f5b467b50d964d5c725183692d2/public/supporters.jpg?raw=true" alt="" />
      </div>
     )
 }
